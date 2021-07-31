fe93749e5ebb4241c23170420a663c171724a07b
Zaid Inamdar <zaidinmd27@gmail.com>
2021-07-31 16:03:39 +0530

    login design

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -5,32 +5,16 @@ import { Card, CardContent, Typography, TextField } from "@material-ui/core";
 function loginPage() {
   return (
     <div>
-      <div className="loginCard">
-        <Card id="materialCard">
-          <CardContent>
-            <div class="container">
-              <div class="row row-cols-2">
-                <div class="row">
-                  <div className="loginForm">
-                    <h1>Student Login</h1>
-                    <form>
-                      <TextField></TextField>
-                    </form>
-                  </div>
-                </div>
-                <div class="col">
-                  <img
-                    src="./abstractBackground.jpg"
-                    alt="..."
-                    style={{
-                      objectFit: "cover",
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+      <div className="head">
+        <h1> Login </h1>
+      </div>
+
+      <img className="wave" src="./wave.png" />
+
+      <div className="container">
+        <div className="img">
+          <img src="./bg.svg" style={{ height: "69vh" }} />
+        </div>
       </div>
     </div>
   );
